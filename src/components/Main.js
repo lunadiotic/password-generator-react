5f3bb15125286686cedbd8c7c283ebadbfa1b131
lionatic <ahmadhakim404@gmail.com>
2022-01-17 16:23:25 +0700

    make a header component with template structure

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './header/Header'
+
+const Main = () => {
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
+
+export default Main
